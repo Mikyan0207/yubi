edb23c88fdf6ec1ab10cbdf06d99f92b59465bba
Florian Theron <mikyan0207@gmail.com>
2021-02-26 23:01:45 +0100

    Get Active/Visible Windows using Windows API with EnumWindows.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,3 +1,33 @@
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+// TODO(Mikyan): Remove std::vector, use custom allocator instead of new.
+BOOL CALLBACK EnumWindowCallback(HWND window, LPARAM lparam)
+{
+    const auto length = GetWindowTextLengthW(window);
+    
+    if (!IsWindowVisible(window) || length == 0)
+        return TRUE;
+    
+    
+    WCHAR* title = new WCHAR[length+1];
+    GetWindowTextW(window, title, length+1);
+    
+    std::vector<std::wstring>& titles = *reinterpret_cast<std::vector<std::wstring>*>(lparam);
+    
+    titles.push_back(title);
+    
+    return TRUE;
+}
+
 int main()
 {
+    // NOTE(Mikyan): Check EnumWindowCallback TODO.
+    std::vector<std::wstring> titles;
+    
+    EnumWindows(EnumWindowCallback, reinterpret_cast<LPARAM>(&titles));
+    
+    for (const auto& title : titles)
+        std::wcout << title << std::endl;
 }
--- build.bat
+++ build.bat
@@ -2,12 +2,13 @@
 
 set CLCompilerPath= "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 
-set opts=-FC -GR- -EHa- -Od -Oi -WX -W4 -nologo -Zi /std:c++17 -Fm"yubi.map"
+set opts=-FC -GR- -EHsc- -Od -Oi -WX -W4 -nologo -Zi /std:c++17 -Fm"yubi.map"
 set code=..\Sources\main.cpp
+set libs=user32.lib
 
 IF NOT EXIST build mkdir build
 
 pushd build
 call %CLCompilerPath% x64
-cl %opts% %code% -Feyubi
+cl %opts% %code% %libs% -Feyubi
 popd
